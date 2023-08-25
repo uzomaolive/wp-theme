f2b842ef549a424cd6444080d0888c6a16ae8643
olive uzoma <olive.c.uzoma@gmail.com>
2023-08-25 17:17:11 +0100

    Added default posts and comments RSS feed links to <head>.

--- wp-content/themes/olive/functions.php
+++ wp-content/themes/olive/functions.php
@@ -1,5 +1,46 @@
 <?php 
 
+if ( ! isset( $content_width ) ) {
+	$content_width = 800; /* pixels */
+}
+
+function olivercakesite_setup() {
+
+    /**
+     * Make theme available for translation.
+     * Translations can be placed in the /languages/ directory.
+     */
+    load_theme_textdomain( 'olivercake', get_template_directory() . '/languages' );
+
+    /**
+     * Add default posts and comments RSS feed links to <head>.
+     */
+    add_theme_support( 'automatic-feed-links' );
+
+    /**
+     * Enable support for post thumbnails and featured images.
+     */
+    add_theme_support( 'post-thumbnails' );
+
+    /**
+     * Add support for two custom navigation menus.
+     */
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
+        'secondary' => __( 'Secondary Menu', 'myfirsttheme' ),
+    ) );
+
+    /**
+     * Enable support for the following post formats:
+     * aside, gallery, quote, image, and video
+     */
+    add_theme_support( 'post-formats', array( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+}
+
+add_theme_support( 'custom-header' );
+
+
+
 function olivercakesite_theme_support(){
 // Adds dynamic title tag support
      add_theme_support('title-tag');
